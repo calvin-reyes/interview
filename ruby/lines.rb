02bf41e06ae2a05be66ce34747b74fb281967dfd
railsdev115 <woodmana994@gmail.com>
2017-12-29 15:30:35 +0200

    Update lines.rb

--- ruby/lines.rb
+++ ruby/lines.rb
@@ -1,35 +1,3 @@
-# Task
-
-# Phase 1
-
-# Generates a blank field with 3 random balls
-def generate_field
-end
-
-# Puts 3 balls in random possitions of the random color to the field
-def generate_random_balls(field)
-end
-
-module UI
-  def put_ball(x,y,color)
-  end
-  def remove_ball(x,y)
-  end
-end
-
-
-# Phase 2
-
-# Moves the ball from cell to cell
-# If line or lines of 5 or more balls of the same color exist
-# Removes completed line or lines
-# Otherwise puts 3 random balls on the field
-def move(from_x, from_y, to_x, to_y)
-end
-
-
-
-# Solution
 
 LINES = [
   # H
